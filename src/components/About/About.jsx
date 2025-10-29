441a11c42b632b9b6722b6b01419c7790a88b300
Vagisha <guptavagisha312@gmail.com>
2025-10-29 15:35:34 +0530

    corrected resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1D1sbuCZ_5p83X7YBPg0pO33Vl_FYrjyJ/view?usp=sharing"
+            href="https://drive.google.com/file/d/1DKbJCZfn0vfAH6n0xG5EhNXCXCQEsNN2/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
