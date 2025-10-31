ee1a2c7ba94355f6a2ec64b24ff4b17167dba65f
Vagisha <guptavagisha312@gmail.com>
2025-10-31 17:33:44 +0530

    corrected cources

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1DKbJCZfn0vfAH6n0xG5EhNXCXCQEsNN2/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1wn8UDBMiOO-EtenlDFNeNd-M70AoXFeW/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
